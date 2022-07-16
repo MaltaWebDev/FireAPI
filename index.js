9d9e752c48e32174e21af063ff473f98be336ea0
MaltaWebDev <MaltaWebDev@gmail.com>
2022-07-16 08:40:35 +0200

    added index js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.get('/api', (req, res) => {
+
+    const apiKey = req.query.apiKey
+
+    // validate api key 
+    // bill user for usage
+
+    res.send({data: '🔥🔥🔥🔥🔥🔥🔥🔥🐐'})
+
+})
+
+app.listen(PORT, () => console.log(`live on http://localhost:${PORT}`))
+     
+
